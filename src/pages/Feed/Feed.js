3530a708ea5f64a7ef9af3aa1ae4e0c70569e830
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-10-12 02:36:23 -0300

    Updating posts on all connected clients

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -41,9 +41,11 @@ class Feed extends Component {
 
     this.loadPosts();
     const socket = openSocket("http://localhost:8080");
-    socket.on('posts', data => {
-      if (data.action === 'create') {
+    socket.on("posts", (data) => {
+      if (data.action === "create") {
         this.addPost(data.post);
+      } else if (data.action === 'update') {
+        this.updatePost(data.post);
       }
     });
   }
@@ -60,6 +62,21 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(
+        (p) => p._id === post._id
+      );
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
       };
     });
   };
@@ -177,25 +194,25 @@ class Feed extends Component {
       })
       .then((resData) => {
         console.log(resData);
-        const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.creator,
-          createdAt: resData.post.createdAt,
-        };
+        // const post = {
+        //   _id: resData.post._id,
+        //   title: resData.post.title,
+        //   content: resData.post.content,
+        //   creator: resData.creator,
+        //   createdAt: resData.post.createdAt,
+        // };
         this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              (p) => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
+          // let updatedPosts = [...prevState.posts];
+          // if (prevState.editPost) {
+          //   const postIndex = prevState.posts.findIndex(
+          //     (p) => p._id === prevState.editPost._id
+          //   );
+          //   updatedPosts[postIndex] = post;
+          // }
           // I removed a else if case here that was updating the posts after creating a new one
           // Since we are using websocket to get new posts now
           return {
-            posts: updatedPosts,
+            // posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
